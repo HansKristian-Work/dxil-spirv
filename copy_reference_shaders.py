3bde05feafdace3329a2c99d5d63084d86d4cb44
Hans-Kristian Arntzen <post@arntzen-software.no>
2020-09-04 16:25:13 +0200

    Support raw copy from DXIL test suite files.

--- copy_reference_shaders.py
+++ copy_reference_shaders.py
@@ -38,15 +38,19 @@ def hashstr(path):
         result = hashlib.sha1(bytes).hexdigest()
     return result
 
-def copy_reference_shader(output_dir, input_path):
-    name = hashstr(input_path)
-    if name is not None:
-        shutil.copy(input_path, os.path.join(output_dir, name + '.dxil'))
+def copy_reference_shader(output_dir, input_path, raw):
+    if raw:
+        shutil.copy(input_path, os.path.join(output_dir, os.path.basename(input_path)))
+    else:
+        name = hashstr(input_path)
+        if name is not None:
+            shutil.copy(input_path, os.path.join(output_dir, name + '.dxil'))
 
 def main():
     parser = argparse.ArgumentParser(description = 'Script for copying VKD3D shader dumps to regression suite.')
     parser.add_argument('--dxil', required = True, help = 'Folder containing a bunch of .dxil shaders.')
     parser.add_argument('--output', required = True, help = 'Output directory.')
+    parser.add_argument('--raw', help = 'Skip hashing. Files must be in format $hash.dxil', action = 'store_true')
 
     args = parser.parse_args()
 
@@ -54,7 +58,7 @@ def main():
         for file in files:
             if os.path.splitext(file)[1] == '.dxil':
                 print('Copying DXIL reference file:', file)
-                copy_reference_shader(args.output, os.path.join(args.dxil, file))
+                copy_reference_shader(args.output, os.path.join(args.dxil, file), args.raw)
 
 if __name__ == '__main__':
     main()
