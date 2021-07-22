0af44092afb3c5b654e7a7f075bf4f7478eaa324
Hans-Kristian Arntzen <post@arntzen-software.no>
2021-07-22 21:28:58 +0200

    Fix DXC build with newer GCC.

--- build_dxc.sh
+++ build_dxc.sh
@@ -13,6 +13,6 @@ fi
 echo "Building DXC."
 mkdir -p external/dxc-build
 cd external/dxc-build
-cmake ../DirectXShaderCompiler -DCMAKE_BUILD_TYPE=$PROFILE -DCMAKE_INSTALL_PREFIX=output $(cat ../DirectXShaderCompiler/utils/cmake-predefined-config-params) -G Ninja
+cmake ../DirectXShaderCompiler -DCMAKE_BUILD_TYPE=$PROFILE -DCMAKE_INSTALL_PREFIX=output $(cat ../DirectXShaderCompiler/utils/cmake-predefined-config-params) -G Ninja -DSPIRV_WERROR=OFF
 cmake --build . --config $PROFILE --target install ${NPROC}
 
