23f001bc26d301a63ebd4666faa4ebd7142a061f
Hans-Kristian Arntzen <post@arntzen-software.no>
2022-02-15 15:52:33 +0100

    Add helper script for roundtripping shaders in vkd3d-proton.

---
+++ roundtrip_shaders.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2022 Hans-Kristian Arntzen for Valve Corporation
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+#
+
+# Helper utility for vkd3d-proton where we roundtrip
+# VKD3D_SHADER_DUMP_PATH -> VKD3D_SHADER_OVERRIDE folders.
+
+import sys
+import os
+import os.path
+import subprocess
+import multiprocessing
+import argparse
+
+def convert_spirv(in_path, out_path, args, stage):
+    out_file = out_path + '.' + stage
+    if args.preserve and os.path.exists(out_file):
+        return
+
+    spirv_cross_cmd = [args.spirv_cross, '--vulkan-semantics', '--output', out_file, in_path, '--stage', stage]
+    try:
+        subprocess.check_call(spirv_cross_cmd, stderr = subprocess.DEVNULL)
+    except subprocess.CalledProcessError as e:
+        if e.returncode > 0:
+            # Ignore this. We kinda expect it.
+            return
+
+def convert_shaders(args):
+    files = os.listdir(args.input)
+    files = [ f for f in files if os.path.splitext(f)[1] == '.spv' ]
+
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    results = []
+
+    for f in files:
+        in_path = os.path.join(args.input, f)
+        out_path = os.path.join(args.output, os.path.splitext(f)[0])
+        # Try to emit all stages. Kinda hacky.
+        # Should maybe add --deduce-extension or something to SPIRV-Cross CLI, but w/e.
+        results.append(pool.apply_async(convert_spirv, args = (in_path, out_path, args, 'vert')))
+        results.append(pool.apply_async(convert_spirv, args = (in_path, out_path, args, 'frag')))
+        results.append(pool.apply_async(convert_spirv, args = (in_path, out_path, args, 'tesc')))
+        results.append(pool.apply_async(convert_spirv, args = (in_path, out_path, args, 'tese')))
+        results.append(pool.apply_async(convert_spirv, args = (in_path, out_path, args, 'geom')))
+        results.append(pool.apply_async(convert_spirv, args = (in_path, out_path, args, 'comp')))
+
+    for res in results:
+        res.get()
+    pool.close()
+    pool.join()
+
+def main():
+    parser = argparse.ArgumentParser(description = 'Script for roundtripping shaders through SPIRV-Cross.')
+    parser.add_argument('--input',
+            help = 'Folder containing shader files to convert.')
+    parser.add_argument('--spirv-cross',
+            default = 'spirv-cross',
+            help = 'Explicit path to SPIRV-Cross')
+    parser.add_argument('--preserve',
+            action = 'store_true',
+            help = 'Do not overwrite files which already exist')
+    parser.add_argument('--output',
+            help = 'Path where shaders are output')
+
+    args = parser.parse_args()
+    if not args.input:
+        sys.stderr.write('Need input shader folder.\n')
+        sys.exit(1)
+    if not args.output:
+        sys.stderr.write('Need output shader folder.\n')
+        sys.exit(1)
+
+    convert_shaders(args)
+    print('Complete!')
+
+if __name__ == '__main__':
+    main()
