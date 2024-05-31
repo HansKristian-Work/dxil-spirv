17de064be5ee9b99d453e44524feaff58b723e39
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-05-31 15:24:33 +0200

    Build DXC with Clang. GCC 14 build is borked for some reason.

--- build_dxc.sh
+++ build_dxc.sh
@@ -14,6 +14,6 @@ echo "Building DXC."
 mkdir -p external/dxc-build
 cd external/dxc-build
 # CLANG_FORMAT_EXE=OFF avoids a broken build where it expects clang-format to produce exact results for some dumb reason.
-cmake ../DirectXShaderCompiler -DCMAKE_BUILD_TYPE=$PROFILE -C ../DirectXShaderCompiler/cmake/caches/PredefinedParams.cmake -G Ninja -DSPIRV_WERROR=OFF -DCLANG_FORMAT_EXE=OFF
+cmake ../DirectXShaderCompiler -DCMAKE_BUILD_TYPE=$PROFILE -C ../DirectXShaderCompiler/cmake/caches/PredefinedParams.cmake -G Ninja -DSPIRV_WERROR=OFF -DCLANG_FORMAT_EXE=OFF -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 cmake --build . --config $PROFILE ${NPROC}
 
