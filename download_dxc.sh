23a6946c94c13a0c85260f7fa778088eb09001ac
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-08-05 11:28:11 +0200

    Remove broken download_dxc.sh.

--- download_dxc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-URL=https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2405/linux_dxc_2024_05_24.x86_64.tar.gz
-
-cd external
-mkdir dxc-build && cd dxc-build
-wget $URL || exit 1
-
-tar xfv linux_dxc_2024_05_24.x86_64.tar.gz
