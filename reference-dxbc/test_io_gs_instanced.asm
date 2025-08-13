b7415af1f62a5a36cb2d2311d2dfcf1e4a6431c2
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-08-13 12:05:07 +0200

    PrimitiveID should go via semantic for DXBC, not op.

--- reference-dxbc/test_io_gs_instanced.asm
+++ reference-dxbc/test_io_gs_instanced.asm
@@ -9,36 +9,37 @@ SPIR-V:
                OpCapability MultiViewport
                OpCapability VulkanMemoryModel
                OpMemoryModel Logical Vulkan
-               OpEntryPoint Geometry %main "main" %SV_POSITION %SV_RenderTargetArrayIndex %SV_ViewportArrayIndex %SV_PrimitiveId %gl_InvocationID %gl_PrimitiveID
+               OpEntryPoint Geometry %main "main" %SV_PrimitiveId %SV_POSITION %SV_RenderTargetArrayIndex %SV_ViewportArrayIndex %SV_PrimitiveId_0 %gl_InvocationID
                OpExecutionMode %main Invocations 12
                OpExecutionMode %main OutputVertices 1
                OpExecutionMode %main InputPoints
                OpExecutionMode %main OutputPoints
                OpName %main "main"
+               OpName %SV_PrimitiveId "SV_PrimitiveId"
                OpName %SV_POSITION "SV_POSITION"
                OpName %SV_RenderTargetArrayIndex "SV_RenderTargetArrayIndex"
                OpName %SV_ViewportArrayIndex "SV_ViewportArrayIndex"
-               OpName %SV_PrimitiveId "SV_PrimitiveId"
+               OpName %SV_PrimitiveId_0 "SV_PrimitiveId"
+               OpDecorate %SV_PrimitiveId BuiltIn PrimitiveId
                OpDecorate %SV_POSITION BuiltIn Position
                OpDecorate %SV_RenderTargetArrayIndex BuiltIn Layer
                OpDecorate %SV_ViewportArrayIndex BuiltIn ViewportIndex
-               OpDecorate %SV_PrimitiveId BuiltIn PrimitiveId
+               OpDecorate %SV_PrimitiveId_0 BuiltIn PrimitiveId
                OpDecorate %gl_InvocationID BuiltIn InvocationId
-               OpDecorate %gl_PrimitiveID BuiltIn PrimitiveId
        %void = OpTypeVoid
           %2 = OpTypeFunction %void
+       %uint = OpTypeInt 32 0
+%_ptr_Input_uint = OpTypePointer Input %uint
+%SV_PrimitiveId = OpVariable %_ptr_Input_uint Input
       %float = OpTypeFloat 32
     %v4float = OpTypeVector %float 4
 %_ptr_Output_v4float = OpTypePointer Output %v4float
 %SV_POSITION = OpVariable %_ptr_Output_v4float Output
-       %uint = OpTypeInt 32 0
 %_ptr_Output_uint = OpTypePointer Output %uint
 %SV_RenderTargetArrayIndex = OpVariable %_ptr_Output_uint Output
 %SV_ViewportArrayIndex = OpVariable %_ptr_Output_uint Output
-%SV_PrimitiveId = OpVariable %_ptr_Output_uint Output
-%_ptr_Input_uint = OpTypePointer Input %uint
+%SV_PrimitiveId_0 = OpVariable %_ptr_Output_uint Output
 %gl_InvocationID = OpVariable %_ptr_Input_uint Input
-%gl_PrimitiveID = OpVariable %_ptr_Input_uint Input
     %uint_12 = OpConstant %uint 12
      %uint_1 = OpConstant %uint 1
 %_ptr_Output_float = OpTypePointer Output %float
@@ -52,14 +53,14 @@ SPIR-V:
                  OpBranch %34
 
          %34 = OpLabel
-         %16 =   OpLoad %uint %gl_InvocationID
-         %18 =   OpLoad %uint %gl_PrimitiveID
+         %17 =   OpLoad %uint %gl_InvocationID
+         %18 =   OpLoad %uint %SV_PrimitiveId
          %19 =   OpIMul %uint %18 %uint_12
-         %21 =   OpIAdd %uint %16 %19
-                 OpStore %SV_PrimitiveId %21
-         %22 =   OpShiftRightLogical %uint %16 %uint_1
+         %21 =   OpIAdd %uint %17 %19
+                 OpStore %SV_PrimitiveId_0 %21
+         %22 =   OpShiftRightLogical %uint %17 %uint_1
                  OpStore %SV_RenderTargetArrayIndex %22
-         %24 =   OpBitwiseAnd %uint %16 %uint_1
+         %24 =   OpBitwiseAnd %uint %17 %uint_1
                  OpStore %SV_ViewportArrayIndex %24
          %26 =   OpAccessChain %_ptr_Output_float %SV_POSITION %uint_0
                  OpStore %26 %float_1
