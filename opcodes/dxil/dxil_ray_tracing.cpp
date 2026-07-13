4b196fc41223c1463240283e510621619479d073
Peter Kohaut <peter.kohaut@gmail.com>
2026-07-13 12:38:46 +0200

    Use indexed loads for LoadLocalRootTableConstant

--- opcodes/dxil/dxil_ray_tracing.cpp
+++ opcodes/dxil/dxil_ray_tracing.cpp
@@ -986,11 +986,13 @@ bool emit_hit_object_load_local_root_table_constant_instruction(Converter::Impl 
 	spv::Id offset = impl.get_id_for_value(instruction->getOperand(2));
 
 	spv::Id uint32 = builder.makeUintType(32);
-	spv::Id uint64 = builder.makeUintType(64);
 
 	if (!impl.hit_object.shader_record_buffer_ptr)
 	{
-		spv::Id srb_struct = builder.makeStructType({ uint32 }, "HitObjectSRB");
+		spv::Id srb_array = builder.makeRuntimeArray(uint32);
+		builder.addDecoration(srb_array, spv::DecorationArrayStride, sizeof(uint32_t));
+
+		spv::Id srb_struct = builder.makeStructType({ srb_array }, "HitObjectSRB");
 
 		builder.addDecoration(srb_struct, spv::DecorationBlock);
 		builder.addDecoration(srb_struct, spv::DecorationHitObjectShaderRecordBufferEXT);
@@ -1006,26 +1008,23 @@ bool emit_hit_object_load_local_root_table_constant_instruction(Converter::Impl 
 	op->add_id(hit_object);
 	impl.add(op);
 
-	auto *cast_op = impl.allocate(spv::OpBitcast, uint64);
+	auto *cast_op = impl.allocate(spv::OpBitcast, builder.makeUintType(64));
 	cast_op->add_id(op->id);
 	impl.add(cast_op);
 
-	auto *convert_op = impl.allocate(spv::OpUConvert, uint64);
-	convert_op->add_id(offset);
+	auto *convert_op = impl.allocate(spv::OpConvertUToPtr, impl.hit_object.shader_record_buffer_ptr);
+	convert_op->add_id(cast_op->id);
 	impl.add(convert_op);
 
-	auto *add_op = impl.allocate(spv::OpIAdd, uint64);
-	add_op->add_id(cast_op->id);
-	add_op->add_id(convert_op->id);
-	impl.add(add_op);
+	auto *index_op = impl.allocate(spv::OpShiftRightLogical, uint32);
+	index_op->add_id(offset);
+	index_op->add_id(builder.makeUintConstant(2));
+	impl.add(index_op);
 
-	convert_op = impl.allocate(spv::OpConvertUToPtr, impl.hit_object.shader_record_buffer_ptr);
-	convert_op->add_id(add_op->id);
-	impl.add(convert_op);
-
-	auto *chain_op = impl.allocate(spv::OpAccessChain, impl.hit_object.shader_record_buffer_member_ptr);
+	auto *chain_op = impl.allocate(spv::OpInBoundsAccessChain, impl.hit_object.shader_record_buffer_member_ptr);
 	chain_op->add_id(convert_op->id);
 	chain_op->add_id(builder.makeUintConstant(0));
+	chain_op->add_id(index_op->id);
 	impl.add(chain_op);
 
 	auto *load_op = impl.allocate(spv::OpLoad, uint32);
@@ -1035,8 +1034,6 @@ bool emit_hit_object_load_local_root_table_constant_instruction(Converter::Impl 
 	impl.add(load_op);
 
 	impl.rewrite_value(instruction, load_op->id);
-	return true;
-
 	return true;
 }
 
